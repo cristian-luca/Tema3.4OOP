89defb847ae01c80d63029bfa4030b88377ff037
Luca Cristian <prof_cristianluca@hotmail.com>
2023-01-29 23:48:33 +0200

    Incarc Tema 3.4

--- src/PigLatin.java
+++ src/PigLatin.java
@@ -1,12 +1,8 @@
 
 public class PigLatin {
     public static Boolean isVowel(char c) {
-        if (c == 'a' || c == 'A' || c == 'e' || c == 'E' || c == 'i' ||
-                c == 'I' || c == 'o' || c == 'O' || c == 'u' || c == 'U') {
-            return true;
-        } else {
-            return false;
-        }
+        return c == 'a' || c == 'A' || c == 'e' || c == 'E' || c == 'i' ||
+                c == 'I' || c == 'o' || c == 'O' || c == 'u' || c == 'U';
     }
 
     public static String findPigLatin(String inp) {
